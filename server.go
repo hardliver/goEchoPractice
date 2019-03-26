62625688ab6b1bd3c2ac8ea8c89d7a8c0f117775
Hardliver <yelionel26@gmail.com>
2019-03-26 17:42:56 +0800

    Basic Authentication Middleware

--- server.go
+++ server.go
@@ -116,6 +116,14 @@ func main() {
     Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
   }))
 
+  g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+    // check in the DB
+    if username == "jack" && password == "1234" {
+      return true, nil
+    }
+    return false, nil
+  }))
+
   g.GET("/main", mainAdmin)
 
 	e.GET("/", yallo)
